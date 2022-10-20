30fcffc77f03da41554910d90b1bde31e122e49f
KML <kaungmyatlin.worklife@gmail.com>
2022-10-20 16:13:07 +0630

    u

--- components/layout/auth/auth_layout.js
+++ components/layout/auth/auth_layout.js
@@ -1,6 +1,9 @@
 import classes from './auth_layout.module.css'
 import Link from 'next/link'
+import { useRef } from 'react'
 const AuthLayout = (props) => {
+    const emInpRef = useRef()
+    const pwInpRef = useRef()
     const authFormSubmitHdl = () => {
         
     }
@@ -8,19 +11,24 @@ const AuthLayout = (props) => {
         <main className={classes.main}>
             <section className={classes.section}>
                 <div className={classes.boxContainer}>
-                    <div className={`${classes.centeringContainer} ${classes.marginBtmSeparator}`}>
+                    <div className={`${classes.centeringContainer} ${classes.containerSeparator}`}>
                         <Link href='/' >
                             <a>
                                 <div className={classes.brand}>Amazing</div>
                             </a>
                         </Link>
                     </div>
-                    <div className={`${classes.marginBtmSeparator} ${classes.centeringContainer} ${classes.featureName}`} style={{marginTop:0}}>
+                    <div className={` ${classes.centeringContainer} ${classes.containerSeparator} ${classes.featureName}`} style={{marginTop:0}}>
                         Login
                     </div>
-                    <form onSubmit={authFormSubmitHdl} className={`${classes.centeringContainer}`}>
-                        <div className={`${classes.marginBtmSeparator}`} >
-                            
+                    <form onSubmit={authFormSubmitHdl} >
+                        <div className={`${classes.containerSeparator} ${classes.fieldContainer}`} >
+                            <label htmlFor='email' className={`${classes.formfieldSeparator}`}>Your Email</label>
+                            <input type='email' id='email' required ref={emInpRef}/>
+                        </div>
+                        <div className={`${classes.containerSeparator}  ${classes.fieldContainer}`} >
+                            <label htmlFor='password' className={`${classes.formfieldSeparator}`}>Your Password</label>
+                            <input type='password' id='password' required ref={pwInpRef}/>
                         </div>
                         <button>{props.formType}</button>
                         <button type='button'>Forgot password?</button>
